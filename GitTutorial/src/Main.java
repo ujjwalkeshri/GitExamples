af4006da6b6601f5e0b83b3524c089783767ce68
Ujjwal <ujjwalkeshri1990@gmail.com>
2023-06-29 22:15:58 +1000

    Emergency-fix

--- GitTutorial/src/Main.java
+++ GitTutorial/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("New change");
+
+        System.out.println("This is changes made as part of emergency fix");
     }
 }
